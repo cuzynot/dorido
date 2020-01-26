663371cf76e5a93e1aa2a2f79e23a2297ee8a13e
Yili Liu <yililiu7@gmail.com>
2020-01-25 19:11:05 -0500

    updated webscraper

--- webscraper.py
+++ webscraper.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import time
 from selenium import webdriver
     
-url = "https://www.loblaws.ca/search?search-bar=dorito"
+url = "https://www.mastermindtoys.com/search/beyblade.aspx"
 
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
@@ -13,41 +13,80 @@ def scrape(url):
     browser.get(url)
     time.sleep(1)
     print("len", len(browser.page_source))
-    curStore = url[12:]
+    curStore = url[url.index(".") + 1:]
     curStore = curStore[:curStore.index(".")]
     print(curStore)
 
     # initialize lists
-    names = []
-    prices = []
-    stores = []
+##    names = []
+##    prices = []
+##    stores = []
     
     allNames = []
     allPrices = []
     allStores = []
+    allRatings = []
 
     # parse html
     soup = BeautifulSoup(browser.page_source, 'html.parser')
     
-    if (curStore == "nofrills" or curStore == "loblaws"):
+    if (curStore == "nofrills" or curStore == "loblaws" or curStore == "fortinos"):
         names = soup.findAll("span", {"class":"product-name__item--name"})
         prices = soup.findAll("span", {"class":"price__value selling-price-list__item__price selling-price-list__item__price--now-price__value"})
-##        names = browser.find_elements_by_class_name("product-name__item--name")
-    elif (curStore == "walmart"):
-        names = soup.findAll("h2")
-        prices = soup.findAll("div", {"possibleRangedCurrencyText: { data: price, simpleFormatting: simpleFormatting } "})
-    elif (curStore == "loblaws"):        
-        names = soup.findAll("span", {"class":"product-name__item product-name__item--name"})
-        prices = soup.findAll("span", {"class":"price__value selling-price-list__item__price selling-price-list__item__price--now-price__value"})
-    
-    # add to lists
-    for item in names:
-        if (item.text != ""):
+
+        for item in names:
             allNames.append(item.text)
 
-    for item in prices:
-        if (item.text != ""):
+        for item in prices:
             allPrices.append(item.text)
+            
+##    elif (curStore == "walmart"):
+##        names = soup.findAll("h2")
+##        prices = soup.findAll("div", {"possibleRangedCurrencyText: { data: price, simpleFormatting: simpleFormatting } "})
+
+
+    elif (curStore == "metro"):        
+        names = soup.findAll("div", {"class":"pt-title"})
+        prices = soup.findAll("div", {"class":"pi--main-price"})
+        
+        for item in names:
+            allNames.append(item.text)
+            
+        for item in prices:
+            allPrices.append(item["data-main-price"])
+
+    elif (curStore == "ebgames"):
+        names = soup.findAll("div", {"class":"singleProdInfo"})
+        prices = soup.findAll("a", {"class":"megaButton cartAddNoRadio"})
+
+        for item in prices:
+            allPrices.append(item.span.b.text)
+            
+        for i in range(len(prices)):
+            allNames.append(names[i].h3.a.text)
+
+    elif (curStore == "toysrus"):
+        names = soup.findAll("div", {"class":"b-product_tile-caption"})
+        prices = soup.findAll("span", {"class":"b-price-value js-sales-price-value"})
+
+        for item in names:
+            allNames.append(item.h2.a.text[2:-2])
+
+        for item in prices:
+            allPrices.append(item.text[2:-2])
+
+    elif (curStore == "mastermindtoys"):
+        names = soup.findAll("span", {"itemprop":"name"})
+        prices = soup.findAll("span", {"itemprop":"price"})
+    
+        for item in names:
+            allNames.append(item.text)
+
+        for item in prices:
+            allPrices.append(item.text)
+
+        
+
 
     for i in range(len(allNames)):
         allStores.append(curStore)
